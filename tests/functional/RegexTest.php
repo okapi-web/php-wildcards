ff7eb9380a4ebbf65d709b6d89eab23c26aedfd9
WalterWoshid <wotschel.valentin@googlemail.com>
2023-02-09 11:55:36 +0100

    Added more test cases

--- tests/functional/RegexTest.php
+++ tests/functional/RegexTest.php
@@ -2,6 +2,7 @@
 
 namespace Okapi\Wildcards\Tests\functional;
 
+use Okapi\Wildcards\Exceptions\WildcardException;
 use Okapi\Wildcards\Regex;
 use PHPUnit\Framework\TestCase;
 
@@ -9,10 +10,10 @@ class RegexTest extends TestCase
 {
     public array $wildcards = [
         // Category
-        '*'  => [
+        '*' => [
             // Should be tested
             'test' => [
-                // Wildcard => Expected result
+                // Wildcard => Expected result (null = exception)
                 'test'  => true,
                 'test2' => false,
                 't*t'   => true,
@@ -23,7 +24,8 @@ class RegexTest extends TestCase
                 '*'     => true,
             ]
         ],
-        '?'  => [
+
+        '?' => [
             'test' => [
                 't??t'  => true,
                 't??t2' => false,
@@ -32,6 +34,7 @@ class RegexTest extends TestCase
                 '?????' => false,
             ]
         ],
+
         '[]' => [
             'test' => [
                 'test[12]'     => true,
@@ -39,27 +42,89 @@ class RegexTest extends TestCase
                 't[e]est'      => true,
                 '[big_]test'   => true,
                 'small_[test]' => false,
+                'test[1'       => null,
             ]
         ],
-        '|'  => [
+
+        '|' => [
             'test' => [
                 'test|test2'          => true,
                 'no_test|broken_test' => false,
             ]
         ],
+
+        '()' => [
+            'test' => [
+                'test(test2)' => false,
+                'test2(test)' => false,
+                '(test)'      => true,
+                '(test'       => null,
+            ]
+        ],
+
+        'mix' => [
+            'test' => [
+                '(test)|(test2)'    => true,
+                '(no_test)|(test2)' => false,
+                '(test)*'           => true,
+                '(test)*2'          => false,
+            ],
+
+            'image.png' => [
+                '(*.png)|(*.jpg)' => true,
+                '(*.php)|(*.js)'  => false,
+                '*.png|*.jpg'     => true,
+                '*.php|*.js'      => false,
+                '*.(png|jpg)'     => true,
+                '*.(php|js)'      => false,
+            ],
+
+            'hello' => [
+                '(hello)|(world)' => true,
+                'hello'           => true,
+                'no_hello'        => false,
+            ],
+
+            'world' => [
+                '(hello)|(world)' => true,
+                'world'           => true,
+                'no_world'        => false,
+            ],
+
+            'index.php' => [
+                'index[*].php' => true,
+                'index[*].js'  => false,
+            ],
+
+            'index.test.php' => [
+                'index[*].php' => true,
+                'index[*].js'  => false,
+            ],
+
+            'index_2.php' => [
+                'index[*].php' => true,
+                'index[*].js'  => false,
+            ],
+        ]
     ];
 
     /**
      * @dataProvider provideFromWildcard
      */
-    public function testFromWildcard(string $test, string $wildcard, bool $expected): void
+    public function testFromWildcard(string $test, string $wildcard, ?bool $expected): void
     {
         $regex = Regex::fromWildcard($wildcard);
-        $this->assertEquals(
-            $expected,
-            $regex->match($test),
-            "Failed to test wildcard '$wildcard' for '$test'."
-        );
+
+        if (gettype($expected) === 'boolean') {
+            $this->assertEquals(
+                $expected,
+                $regex->match($test),
+                "Failed to test wildcard '$wildcard' for '$test'."
+            );
+        } else {
+            $this->expectException(WildcardException::class);
+            $regex->match($test);
+        }
     }
 
     public function provideFromWildcard(): array
